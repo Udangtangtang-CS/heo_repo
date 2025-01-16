1d973c127d7ebab89ae846f857693a40dc5750fc
jiyoungmerong <jiyoungmerong@gmail.com>
2025-01-16 23:53:27 +0900

    문자열 돌리기

---
+++ Jan16/TwistTheString.java
@@ -0,0 +1,13 @@
+package Jan16;
+
+import java.util.Scanner;
+
+public class TwistTheString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.next().split("");
+        for(String a : arr){
+            System.out.println(a);
+        }
+    }
+}
