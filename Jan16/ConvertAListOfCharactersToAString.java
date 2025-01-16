9e836a40212dd4c45d14560c143b83bb39a971a7
jiyoungmerong <jiyoungmerong@gmail.com>
2025-01-16 23:55:13 +0900

    문자열 리스트를 문자열로 변환하기

---
+++ Jan16/ConvertAListOfCharactersToAString.java
@@ -0,0 +1,12 @@
+package Jan16;
+
+class ConvertAListOfCharactersToAString {
+    public String solution(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for(String str : arr){
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
