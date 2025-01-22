d6edc570e762c2f08fe61767441374297b9eb2be
jiyoungmerong <jiyoungmerong@gmail.com>
2025-01-22 22:26:33 +0900

    카운트 업

---
+++ Jan22/CountUp.java
@@ -0,0 +1,18 @@
+package Jan22;
+
+/*
+
+[ 문제 설명 ]
+정수 start_num와 end_num가 주어질 때,
+start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return 하도록 solution 함수를 완성해주세요.
+
+ */
+public class CountUp {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num-(start_num-1)];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = start_num + i;
+        }
+        return answer;
+    }
+}
