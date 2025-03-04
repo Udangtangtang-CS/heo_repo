e0fa9b5d02b62d16b3f7fc2ece3b88687e2db143
jiyoungmerong <jiyoungmerong@gmail.com>
2025-03-04 22:21:49 +0900

    9로 나눈 나머지

---
+++ Mar04/RemainderWhenDividedBy9.java
@@ -0,0 +1,16 @@
+package Mar04;
+
+/*
+
+[ 문제 설명 ]
+
+음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때,
+이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+
+ */
+public class RemainderWhenDividedBy9 {
+    public int solution(String number) {
+        return number.chars().map(c -> (c - '0') % 9).sum() % 9;
+    }
+}
