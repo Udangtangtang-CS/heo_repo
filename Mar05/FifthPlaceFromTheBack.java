08f37a58c046e1ea22cb64b437a16aa315961b9f
jiyoungmerong <jiyoungmerong@gmail.com>
2025-03-05 21:44:33 +0900

    뒤에서 5등 위로

---
+++ Mar05/FifthPlaceFromTheBack.java
@@ -0,0 +1,19 @@
+package Mar05;
+
+import java.util.Arrays;
+
+/*
+
+[ 문제 설명 ]
+
+정수로 이루어진 리스트 num_list가 주어집니다.
+num_list에서 가장 작은 5개의 수를 제외한 수들을 오름차순으로 담은 리스트를
+return하도록 solution 함수를 완성해주세요.
+
+ */
+public class FifthPlaceFromTheBack {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
+    }
+}
